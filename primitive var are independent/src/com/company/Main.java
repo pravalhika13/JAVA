8260c6bf1717f3286a76f919bf54cff28ba41542
pravalhika13 <42563201+pravalhika13@users.noreply.github.com>
2020-02-15 15:29:17 -0600

    commit
    commit

---
+++ primitive var are independent/src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+import java.util.Date;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        byte x=1;
+        byte y=x;
+        x=2;
+        System.out.println(y);
+    }
+}
